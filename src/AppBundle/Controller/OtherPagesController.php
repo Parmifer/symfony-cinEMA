3ef14e8495385b472cb4aac6b28d8b2ead0118ad
Parmifer <yannbutscher@gmail.com>
2017-02-15 12:12:21 +0100

    Ajout d'un accueil. Correction des bêtises de Lucile pour 1_insert.sql.

--- src/AppBundle/Controller/OtherPagesController.php
+++ src/AppBundle/Controller/OtherPagesController.php
@@ -6,23 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class OtherPagesController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render('AppBundle:OtherPages:index.html.twig', [
+            
+        ]);
+    }
+    
     public function showAboutAction()
     {             
-        $createurs = array(
+        $createurs = [
             'Yann BUTSCHER',
             'Lucile DECROZANT-TRIQUENAUX'
-        );
+        ];
         
-        return $this->render('AppBundle:OtherPages:about.html.twig', array(
+        return $this->render('AppBundle:OtherPages:about.html.twig', [
             'createurs' => $createurs
-        ));
+        ]);
     }
 
     public function showLegalMentionsAction()
     {
-        return $this->render('AppBundle:OtherPages:legal_mentions.html.twig', array(
+        return $this->render('AppBundle:OtherPages:legal_mentions.html.twig', [
             // ...
-        ));
+        ]);
     }
-
 }
