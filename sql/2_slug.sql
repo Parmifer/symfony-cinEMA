b1c8c9cc28f54af9593e7a023f855e5a723900ca
Parmifer <yannbutscher@gmail.com>
2017-02-16 15:40:46 +0100

    Insert slug.

---
+++ sql/2_slug.sql
@@ -0,0 +1,6 @@
+use tp_symfony_cinema;
+
+update film set slug='une-histoire-d-eau' where id=1;
+update film set slug='a-bout-de-souffle' where id=2;
+update film set slug='la-strada' where id=3;
+update film set slug='dolce-vita' where id=4;
